89656f9c50fe5da2140fc68b1d77136c97bb45ee
hoailp <phuochoaileqn@gmail.com>
2024-09-17 17:45:08 +0700

    feat: sui multisig sdk

---
+++ src/sui_multisig.cpp
@@ -0,0 +1,21 @@
+#include "sui_multisig.h"
+#include "header/sui_lib.h"
+
+MultiSig SuiMultisig::getOrCreateMultisig(CStringArray addresses, CU8Array weights, uint16_t threshold) {
+	return get_or_create_multisig(addresses, weights, threshold);
+}
+
+CU8Array SuiMultisig::createTransaction(const char *from_address,
+		const char *to_address, uint64_t amount) {
+	return create_transaction(from_address, to_address, amount);
+}
+
+const char *SuiMultisig::signAndExecuteTransaction(CU8Array multisig,
+		CU8Array tx,
+		CStringArray addresses) {
+	return sign_and_execute_transaction(multisig, tx, addresses);
+}
+
+void SuiMultisig::freeMultisig(MultiSig multisig) {
+	free_multisig(multisig);
+}
