da281f48afb1e0222367b82e991c7e315b58b5c4
Hoan Le Huu <hoanle396@gmail.com>
2024-09-30 15:18:44 +0700

    feat: update wallet demo UI

--- modules/sui_sdk/tests/test_sui_balance.h
+++ modules/sui_sdk/tests/test_sui_balance.h
@@ -9,31 +9,33 @@
 using namespace godot;
 namespace TestSuiBalanceSDK
 {
+	String wallet = "0x0d000320903ccdb00281bc30a568af9b546ec4ef8d748f352f8c31ad08442fdb";
+
 	TEST_CASE("Get balance sync")
 	{
 		SuiSDK suiSdk;
-		Ref<BalanceWrapper> balance = suiSdk.getBalanceSync("0x0d000320903ccdb00281bc30a568af9b546ec4ef8d748f352f8c31ad08442fdb");
+		Ref<BalanceWrapper> balance = suiSdk.getBalanceSync(wallet);
 		CHECK(balance->get_total_balance() != "");
 	}
 
 	TEST_CASE("Get balances")
 	{
 		SuiSDK suiSdk;
-		TypedArray<BalanceWrapper> balances = suiSdk.getBalances("0x0d000320903ccdb00281bc30a568af9b546ec4ef8d748f352f8c31ad08442fdb");
+		TypedArray<BalanceWrapper> balances = suiSdk.getBalances(wallet);
 		CHECK(balances.size() != (const int)0);
 	}
 
 	TEST_CASE("Get All balance sync")
 	{
 		SuiSDK suiSdk;
-		TypedArray<BalanceWrapper> balances = suiSdk.getAllBalancesSync("0x0d000320903ccdb00281bc30a568af9b546ec4ef8d748f352f8c31ad08442fdb");
+		TypedArray<BalanceWrapper> balances = suiSdk.getAllBalancesSync(wallet);
 		CHECK(balances.size() != (const int)0);
 	}
 
 	TEST_CASE("Get coin sync")
 	{
 		SuiSDK suiSdk;
-		TypedArray<CoinWrapper> coins = suiSdk.getCoinsSync("0x0d000320903ccdb00281bc30a568af9b546ec4ef8d748f352f8c31ad08442fdb");
+		TypedArray<CoinWrapper> coins = suiSdk.getCoinsSync(wallet);
 		CHECK(coins.size() != (const int)0);
 	}
 
