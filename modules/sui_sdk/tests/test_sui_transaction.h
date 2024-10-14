6e1dbdd782a9e76db97d764bbf25ee16d25917cb
Hoan Le Huu <hoanle396@gmail.com>
2024-10-14 17:28:57 +0700

    feat: update test case

--- modules/sui_sdk/tests/test_sui_transaction.h
+++ modules/sui_sdk/tests/test_sui_transaction.h
@@ -124,8 +124,34 @@ namespace TestSuiTransactionSDK
       sleep(5);
     } while (enoughAmount == false);
 
-    
-    String result = suiSDK.programmableTransactionBuilder(sender->address, recipient->address, amount);
+    SuiProgrammableTransactionBuilder *builder = memnew(SuiProgrammableTransactionBuilder);
+    SuiArguments *coin = memnew(SuiArguments);
+    suiSDK.addArgumentGasCoin(coin);
+
+    SuiArguments *amountArg = memnew(SuiArguments);
+    SuiBSCBasic *amountBscBasic = memnew(SuiBSCBasic);
+
+    amountBscBasic->BSCBasic("u64", String::num_uint64(amount));
+
+    suiSDK.makePure(builder, amountArg, amountBscBasic);
+    suiSDK.addSplitCoinsCommand(builder, coin, amountArg);
+
+    SuiArguments *argument = memnew(SuiArguments);
+    suiSDK.addArgumentResult(argument, 0);
+    SuiArguments *recipientArg = memnew(SuiArguments);
+    SuiBSCBasic *recipientBscBasic = memnew(SuiBSCBasic);
+    recipientBscBasic->BSCBasic("address", recipient->address);
+    suiSDK.makePure(builder, recipientArg, recipientBscBasic);
+    suiSDK.addTransferObjectCommand(builder, argument, recipientArg);
+
+    String result = suiSDK.executeTransaction(builder, sender->address, gas);
+    memdelete(builder);
+    memdelete(amountArg);
+    memdelete(amountBscBasic);
+    memdelete(argument);
+    memdelete(recipientArg);
+    memdelete(recipientBscBasic);
+
     CHECK(result != "");
   }
 
@@ -145,6 +171,64 @@ namespace TestSuiTransactionSDK
     String result = suiSDK.signTransaction(sender->address, recipient->address, amount);
     CHECK(result == "Error: Transaction failed");
   }
+  TEST_CASE("Sign transaction builder allow sponser")
+  {
+    TypedArray<WalletWrapper> wallets = suiSDK.getWallets();
+    while (wallets.size() < 3)
+    {
+      suiSDK.generateAndAddKey();
+      wallets = suiSDK.getWallets();
+    }
+
+    Ref<WalletWrapper> sender = wallets[0];
+    Ref<WalletWrapper> recipient = wallets[1];
+    Ref<WalletWrapper> sponser = wallets[2];
+    uint64_t amount = pow(10, 9);
+    uint64_t gas = 0.005 * pow(10, 9);
+
+    bool enoughAmount = false;
+    do
+    {
+      Ref<BalanceWrapper> balance = suiSDK.getBalanceSync(sponser->address);
+      enoughAmount = stoull(balance->total_balance.utf8().get_data()) >= amount + gas;
+      if (!enoughAmount)
+      {
+        suiSDK.requestTokensFromFaucet(sponser->address);
+      }
+      sleep(5);
+    } while (enoughAmount == false);
+
+    SuiProgrammableTransactionBuilder *builder = memnew(SuiProgrammableTransactionBuilder);
+    SuiArguments *coin = memnew(SuiArguments);
+    suiSDK.addArgumentGasCoin(coin);
+
+    SuiArguments *amountArg = memnew(SuiArguments);
+    SuiBSCBasic *amountBscBasic = memnew(SuiBSCBasic);
+
+    amountBscBasic->BSCBasic("u64", String::num_uint64(amount));
+
+    suiSDK.makePure(builder, amountArg, amountBscBasic);
+    suiSDK.addSplitCoinsCommand(builder, coin, amountArg);
+
+    SuiArguments *argument = memnew(SuiArguments);
+    suiSDK.addArgumentResult(argument, 0);
+    SuiArguments *recipientArg = memnew(SuiArguments);
+    SuiBSCBasic *recipientBscBasic = memnew(SuiBSCBasic);
+    recipientBscBasic->BSCBasic("address", recipient->address);
+    suiSDK.makePure(builder, recipientArg, recipientBscBasic);
+    suiSDK.addTransferObjectCommand(builder, argument, recipientArg);
+
+    String result = suiSDK.executeTransactionAllowSponser(builder, sender->address, gas, sponser->address);
+    memdelete(builder);
+    memdelete(amountArg);
+    memdelete(amountBscBasic);
+    memdelete(argument);
+    memdelete(recipientArg);
+    memdelete(recipientBscBasic);
+
+    CHECK(result != "");
+  }
+
 }
 
 #endif
